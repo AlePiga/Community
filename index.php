f1532db84863518f00032b75ab158cde5f7e1b9d
AlePiga <alessandro07.pigaiani@gmail.com>
2025-09-27 11:17:14 +0200

    pì acca pì

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <title>Console Retro</title>
+    <title>Retro Console Community</title>
   </head>
   <body class="select-none bg-gray-50 text-gray-800 antialiased">
     <div class="max-w-4xl mx-auto p-6">
-      <h1 class="text-5xl font-semibold text-center mt-3 mb-6">
-        Test community
+      <h1 class="text-5xl font-semibold text-center mt-3 mb-8">
+        Retro Console Community
       </h1>
-      <!-- <p class="text-1 text-center mb-6">Rivivi la magia dei giochi classici: la tua community per console retro!</p> -->
-      <div class="overflow-x-auto bg-white rounded-xl shadow">
+        <!-- <p class="text-1 text-center mb-6">Rivivi la magia dei giochi classici: la tua community per console retro!</p>-->
+        <div class="overflow-x-auto bg-white rounded-xl shadow">
         <table id="infoTable" class="min-w-full divide-y divide-gray-200">
           <thead class="bg-gray-100"></thead>
           <tbody class="bg-white divide-y divide-gray-100"></tbody>
