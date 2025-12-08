a65f0f8345b6b02412cc5e4e07bb9cdc13d0357e
AlePiga <134812981+AlePiga@users.noreply.github.com>
2025-12-08 17:08:05 +0100

    order by interprete

--- views/home.php
+++ views/home.php
@@ -2,7 +2,7 @@
 include '../model/Database.php';
 
 $database = new Database();
-$cds = $database->query("SELECT * FROM cds")->fetch_all(MYSQLI_ASSOC);
+$cds = $database->query("SELECT * FROM cds ORDER BY Interprete")->fetch_all(MYSQLI_ASSOC);
 function stelline($numero)
 {
   $stellaPiena = '<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-yellow-400 inline" fill="currentColor" viewBox="0 0 20 20"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.95a1 1 0 00.95.69h4.15c.969 0 1.371 1.24.588 1.81l-3.36 2.44a1 1 0 00-.364 1.118l1.287 3.951c.3.921-.755 1.688-1.54 1.118l-3.36-2.44a1 1 0 00-1.175 0l-3.36 2.44c-.785.57-1.84-.197-1.54-1.118l1.287-3.951a1 1 0 00-.364-1.118L2.075 9.377c-.783-.57-.38-1.81.588-1.81h4.15a1 1 0 00.95-.69l1.286-3.95z"/></svg>';
